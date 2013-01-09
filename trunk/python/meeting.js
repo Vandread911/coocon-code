5da21d5cc333b1f3a47cd3ee23186a3cde5c38f7
coocon2007@gmail.com <coocon2007@gmail.com@7f4fc389-6b06-b884-9ef8-0cbc7884ba6d>
2013-01-09 18:27:58 +0000

    hi add  增加注释哇。。。。

--- trunk/python/meeting.js
+++ trunk/python/meeting.js
@@ -1,3 +1,12 @@
+/**
+* 
+* @file 主要用来辅助预订预订会议室,
+* 1.查询到所需的会议室(一般是还未到开放预订时间)
+* 2.启动meeting.js 新建一个代理server
+* 3.浏览器设置代理地址为 此server地址
+* 4.点击预订 此时如果没有预订成功 会一直重复提交预订请求，直到成功
+* @author gengpeng@baidu.com
+*/
 var net = require('net');
 var fs = require('fs');
 var local_port = 9998;
@@ -9,16 +18,17 @@ var tryTimesDic = [];
 
 //在本地创建一个server监听本地local_port端口
 //
-net.createServer(function (client)
-{
+net.createServer(function(client) {
     
     //首先监听浏览器的数据发送事件，直到收到的数据包含完整的http请求头
     var buffer = new Buffer(0);
     var _userName = userName;
-    client.on('data',function(data, reqq)
-    {
-        buffer = buffer_add(buffer,data);
-        if (buffer_find_body(buffer) == -1)  {
+
+
+    client.on('data',function(data, req) {
+    
+        buffer = buffer_add(buffer, data);
+        if (buffer_find_body(buffer) == -1) {
             return;
         }
         var req = parse_request(buffer);
@@ -31,16 +41,23 @@ net.createServer(function (client)
     });
 
     //从http请求头部取得请求信息后，继续监听浏览器发送数据，同时连接目标服务器，并把目标服务器的数据传给浏览器
-    function relay_connection(req, userName)
-    {
+
+    /**
+    * @param {Object} req request的头部信息
+    * @param {string} userName 需要匹配特定的用户才能预订
+    */
+    function relay_connection(req, userName) {
+    
         var arr = [];
 
         //如果请求不是CONNECT方法（GET, POST），那么替换掉头部的一些东西
         if (req.method != 'CONNECT') {
             //先从buffer中取出头部
             var _body_pos = buffer_find_body(buffer);
-            if (_body_pos < 0) _body_pos = buffer.length;
-            var header = buffer.slice(0,_body_pos).toString('utf8');
+            if (_body_pos < 0) {
+                _body_pos = buffer.length;
+            }
+            var header = buffer.slice(0, _body_pos).toString('utf8');
             //替换connection头
             header = header.replace(/(proxy\-)?connection\:.+\r\n/ig,'')
                     .replace(/Keep\-Alive\:.+\r\n/i,'')
@@ -48,23 +65,27 @@ net.createServer(function (client)
             //替换网址格式(去掉域名部分)
             if (req.httpVersion == '1.1') {
                 var url = req.path.replace(/http\:\/\/[^\/]+/,'');
-                if (url.path != url) header = header.replace(req.path,url);
+                if (url.path != url) {
+                    header = header.replace(req.path,url);
+                }
             }
-            buffer = buffer_add(new Buffer(header,'utf8'),buffer.slice(_body_pos));
+            buffer = buffer_add(new Buffer(header,'utf8'), buffer.slice(_body_pos));
         }
-        // 扩充id he userName
+        // 扩充id 和 userName
         var strReq = logs.reqShow(req, userName); 
+
         //普通的发送请求
         createServer(client, req);
         //如果是标识    请求 就重复发送
-        if(req.roomId && req.userName) {
+        if (req.roomId && req.userName) {
             logs.show(strReq);
             
             reqDic[req.roomId] = setInterval(function() {
                 createServer_repeat(client, req, reqDic);
-                if(tryTimesDic[req.roomId]) {
+                if (tryTimesDic[req.roomId]) {
                     tryTimesDic[req.roomId] ++;
-                }else {
+                }
+                else {
                     tryTimesDic[req.roomId] = 1;
                 }
                 console.log('we have a try roomId:', req.roomId,'  times:', tryTimesDic[req.roomId] ); 
@@ -78,42 +99,40 @@ net.createServer(function (client)
         function createServer(client, req) {
         
             //建立到目标服务器的连接
-            var server = net.createConnection(req.port,req.host, function(ls) {
+            var server = net.createConnection(req.port, req.host, function(ls) {
                 if (req.method == 'CONNECT') {
-                    client.write(new Buffer("HTTP/1.1 200 Connection established\r\nConnection: close\r\n\r\n"));
-                } else {
+                    client.write(
+                        new Buffer("HTTP/1.1 200 Connection established\r\nConnection: close\r\n\r\n")
+                    );
+                } 
+                else {
                   
                     server.write(buffer);
                 } 
             
                 //交换服务器与浏览器的数据
-                client.on("data", function(data){
-                    try {
-                        server.write(data); 
-                        console.log('data exchange form client and server');
-                    } catch(ex) {
-                        console.log('error: write server error'); 
-                    }
+                client.on("data", function(data) {
+                    server.write(data); 
+                    console.log('data exchange form client and server');
                 });
         
-                server.on("data", function(data){ 
+                server.on("data", function(data) { 
                     //this is our right request o~~~~~~~
                     var str = data.toString('utf8');
-                    if(req.userName &&  str.match(/true/g) ){
-                        console.log('meeting   book   ok................................');
+                    if (req.userName &&  str.match(/true/g)) {
+                        console.log('meeting   book   ok.................');
                     }
-                    try {
-                        client.write(data); 
-                    } catch(ex) {
-                        console.log('error on write clien'); 
-                    } 
+                    client.write(data); 
                 });
 
             });
         } 
       
         /**
-         * according request create a socket
+         * according request create a socket 重复请求的一个过程，直到预订成功
+         * @param {Object} client  客户端
+         * @param {Object} req  请求request对象
+         * @param {Object} reqDic  请求信息的字典 
          *
          */
         function createServer_repeat(client, req, reqDic) {
@@ -123,34 +142,37 @@ net.createServer(function (client)
             //建立到目标服务器的连接
             var server = net.createConnection(req.port,req.host, function(ls) {
                 if (req.method == 'CONNECT') {
-                 //   client.write(new Buffer("HTTP/1.1 200 Connection established\r\nConnection: close\r\n\r\n"));
-                } else {
+                    client.write(new Buffer("HTTP/1.1 200 Connection established\r\nConnection: close\r\n\r\n"));
+                }
+                else {
                     server.write(buffer);
                 } 
-                server.on("data", function(data){ 
+                server.on("data", function(data) { 
                     //this is our right request o~~~~~~~
                     var str = data.toString('utf8');
-                    if(req.userName &&  str.match(/true/g) ){
+                    if (req.userName && str.match(/true/g)) {
 
-                        console.log('meeting room： ' + req.roomId + '  book   ok.........................................');
-                        clearInterval( reqDic[roomId]);
+                        console.log(
+                            'meeting room： ' 
+                            + req.roomId 
+                            + '  book   ok...................'
+                        );
+                        clearInterval(reqDic[roomId]);
                     }
                   
                 });
 
             });
         } 
-     
        
     }
 }).listen(local_port);
 
-console.log('Proxy server running at localhost:'+local_port);
+console.log('Proxy server running at localhost:' + local_port);
 
 
 //处理各种错误
-process.on('uncaughtException', function(err)
-{
+process.on('uncaughtException', function(err) {
     console.log("\nError!!!!");
     console.log(err);
 });
@@ -162,22 +184,26 @@ process.on('uncaughtException', function(err)
 * 如果是 CONNECT 方法，那么会返回 { method,host,port,httpVersion}
 * 如果是 GET/POST 方法，那么返回 { metod,host,port,path,httpVersion}
 */
-function parse_request(buffer)
-{
+function parse_request(buffer) {
+
     var s = buffer.toString('utf8');
     var method = s.split('\n')[0].match(/^([A-Z]+)\s/)[1];
     //console.log(s);
-    if (method == 'CONNECT')
-    {
+    if (method == 'CONNECT') {
+    
         var arr = s.match(/^([A-Z]+)\s([^\:\s]+)\:(\d+)\sHTTP\/(\d\.\d)/);
-        if (arr && arr[1] && arr[2] && arr[3] && arr[4])
-            return { method: arr[1], host:arr[2], port:arr[3],httpVersion:arr[4] };
+        if (arr && arr[1] && arr[2] && arr[3] && arr[4]) {
+            return {
+                method: arr[1],
+                host:arr[2],
+                port:arr[3],
+                httpVersion:arr[4] 
+            };
+        }
     }
-    else
-    {
+    else {
         var arr = s.match(/^([A-Z]+)\s([^\s]+)\sHTTP\/(\d\.\d)/);
-        if (arr && arr[1] && arr[2] && arr[3])
-        {
+        if (arr && arr[1] && arr[2] && arr[3]) {
             var host = s.match(/Host\:\s+([^\n\s\r]+)/)[1];
             var obj = {};
             obj.method = arr[1];
@@ -188,19 +214,17 @@ function parse_request(buffer)
                 obj.port = _p[1]?_p[1]:80;
                 obj.path = arr[2];
                 obj.httpVersion = arr[3];
-                if(param) {
-                    if( typeof(param) != "string" ) {
+                if (param) {
+                    if (typeof(param) != "string") {
                         obj.param = typeof (param); 
-                    } else {
-                    
+                    }
+                    else {
                         obj.param = param;
                     }
                 }
            
                 return obj;
-
             }
-            
         }
     }
     return false;
@@ -212,8 +236,8 @@ function parse_request(buffer)
 /**
 * 两个buffer对象加起来
 */
-function buffer_add(buf1,buf2)
-{
+function buffer_add(buf1, buf2) {
+
     var re = new Buffer(buf1.length + buf2.length);
     buf1.copy(re);
     buf2.copy(re,buf1.length);
@@ -223,75 +247,93 @@ function buffer_add(buf1,buf2)
 /**
 * 从缓存中找到头部结束标记("\r\n\r\n")的位置
 */
-function buffer_find_body(b)
-{
-    for(var i=0,len=b.length-3;i<len;i++)
-    {
-        if (b[i] == 0x0d && b[i+1] == 0x0a && b[i+2] == 0x0d && b[i+3] == 0x0a)
-        {
-            return i+4;
+function buffer_find_body(b) {
+
+    for (var i = 0, len = b.length - 3; i < len; i ++) {
+    
+        if (b[i] == 0x0d && b[i+1] == 0x0a && b[i+2] == 0x0d && b[i+3] == 0x0a) {
+        
+            return i + 4;
         }
     }
     return -1;
 }
 
-//log实例 
+/**
+* @return {Object} obj
+* @return {Function()} obj.reqShow  截获敏感的request
+* @return {Function(string)} obj.save  log存储到文件
+* @return {Function(string)} obj.logShow log的显示函数
+*/
 function getLogs() {
+    /**
+    * @return {Object}  obj
+    * @return {Function(Object)} obj.getItem
+    * @return {Function(Object)} obj.addItem
+    */
     var reqTool = function() {
-
         var reqDic = {};
         var value = 1;
         return {
             getItem: function(req) {
                 var str = encodeURIComponent(req.toString('utf8'));              
-                if(reqDic[str]) {
+                if (reqDic[str]) {
                     return reqDic[str];
-                }else {
+                }
+                else {
                     return null; 
                 }
             },
             addItem: function(req) {
                 var str = encodeURIComponent(req.param.toString('utf8'));              
-                if(!reqDic[str]) {
+                if (!reqDic[str]) {
                     reqDic[str] = value ++;
                 }         
                 console.log(reqDic[str], str);
                 return reqDic[str];
             }
-        
         }
     }();
+
+    /**
+    * @param {Object} req 请求信息
+    * @param {string} userName 验证的名字
+    * @return {string} 成功后的log信息字符串
+    */
     var reqShow =  function(req, userName) {
+        //貌似try不顶用 异步的try 在失败的时候执行环境没了？
         try {
-            var regUrl  = /graniteamf\/amf/g;
+            var regUrl = /graniteamf\/amf/g;
             var arr = [];
             var _userName = userName || /gengpeng/g;
             
-            if(!req.path || !req.path.match(regUrl)) { 
+            if (!req.path || !req.path.match(regUrl)) { 
                return null;  
             }
-            if(!req.param || !req.param.match(_userName)) {
+            if (!req.param || !req.param.match(_userName)) {
                 return null; 
             }
 
             arr.push(' ----start  reqeust----');
-            for(var i in req) {
+            for (var i in req) {
                 arr.push(i + ":" + req[i]); 
             }
             arr.push('----end  reqeust------' + new Date() + '\n\n\n');
-
            
             var id = reqTool.addItem(req);
             
             req.roomId = id;
             req.userName = _userName;
             return arr.join('\t');
-        }catch(ex) {
+        } catch(ex) {
             console.log(ex  + '---error----\n');   
             return  null;
         } 
-    },
-    logSave = function(str) {
+    };
+    /**
+    * @param {string} str 存入文件的str
+    */
+    var logSave = function(str) {
         var end = '\n';
         str = str || end;
         fs.appendFile('logs.txt', str , function (err) {
@@ -299,8 +341,11 @@ function getLogs() {
             console.log('The "data to append" was appended to file!-------------------------ok');
         });
     
-    },
-    logShow = function(str) {
+    };
+    /**
+    * @param {string} str 格式化显示的log信息
+    */
+    var logShow = function(str) {
         var end = '  ' + new Date() + '\n';
         str = (str + end) || end; 
         console.log(str); 
@@ -310,7 +355,5 @@ function getLogs() {
         reqShow: reqShow,
         save: logSave,
         show: logShow
-         
     }
-
 }
